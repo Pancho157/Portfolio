a64ce1a4f624ee2318226eb5b56e616fe76f3daa
Francisco Muñiz <hoxocraft65@gmail.com>
2022-07-18 08:24:54 -0300

    Generado el archivo que habilita y desabilita los botones

--- JS/EnableDisableButtons.js
+++ JS/EnableDisableButtons.js
@@ -1,11 +1,26 @@
 function toggleDisabledButtons(disabled) {
-  if (disabled) {
-    document.querySelectorAll(".algorithm__option").disabled = true;
-    document.querySelectorAll(".algorithm__columnsQuantity").disabled = true;
+  let bubbleSortButton = document.getElementById("bubbleSort");
+  let selectionSortButton = document.getElementById("selectionSort");
+  let insertionSortButton = document.getElementById("insertionSort");
+  let quickSortButton = document.getElementById("quickSort");
+  let colsRange = document.getElementById("colsQuantity");
+  let speedRange = document.getElementById("speedValue");
+
+  if (disabled === true) {
+    bubbleSortButton.disabled = true;
+    selectionSortButton.disabled = true;
+    insertionSortButton.disabled = true;
+    quickSortButton.disabled = true;
+    colsRange.disabled = true;
+    speedRange.disabled = true;
   } else {
-    document.querySelectorAll(".algorithm__option").disabled = false;
-    document.querySelectorAll(".algorithm__columnsQuantity").disabled = false;
+    bubbleSortButton.disabled = false;
+    selectionSortButton.disabled = false;
+    insertionSortButton.disabled = false;
+    quickSortButton.disabled = false;
+    colsRange.disabled = false;
+    speedRange.disabled = false;
   }
 }
 
-toggleDisabledButtons(true);
+toggleDisabledButtons(false);
