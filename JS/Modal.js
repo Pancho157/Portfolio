f61f4bfe4bc8604ec0d142d926d203855c9f9e3f
Francisco Muñiz <hoxocraft65@gmail.com>
2022-09-19 10:47:32 -0300

    Cambiar el getElementByID del modal por un eventListener - No agarra por id debido a que se genera de manera dinámica

--- JS/Modal.js
+++ JS/Modal.js
@@ -1,11 +1,3 @@
-var modal = document.getElementById("modal");
-var modalOpenButton = document.getElementsByClassName("projectModal")[0];
-var closeModalButton = document.getElementById("modal__closeBtn");
-
-modalOpenButton.addEventListener("click", openModal);
-closeModalButton.addEventListener("click", closeModal);
-window.addEventListener("click", outsideClick);
-
 function closeModal() {
   modal.style.display = "none";
 }
@@ -16,6 +8,45 @@ function outsideClick(e) {
   }
 }
 
-function openModal() {
+function openModal(index) {
+  var modal = document.getElementById("modal");
+  modal.innerHTML = "";
+
+  const modalHTML = `
+    <div class="projects__modalContent">
+      <span class="modal__closeBtn" id="modal__closeBtn">&times;</span>
+
+        <img src="${projects[index].imgURL}" alt="Imagen del proyecto" class="projects__modalImg"/>
+
+        <div class  ="projects__modalRight">
+
+          <h3 class="projects__projectTitle">API REST</h3>
+
+          <div class="projects__projectRedirections">
+            <a href="${projects[index].webPage}" class="projectLink__left projects__projectLink">Web Page</a>
+
+            <a href="${projects[index].github}" class="projectLink__right projects__projectLink">GitHub</a>
+          </div>
+
+          <hr />
+
+          <p class="projects__projectText projects__centerText">This project uses: </p>
+
+          <p class="projects__projectText projects__centerText">
+            ${projects[index].technologies}
+          </p>
+
+          <hr />
+
+          <p class="projects__projectText">
+            ${projects[index].description}
+          </p>
+        </div>
+      </div>
+  `;
+
+  modal.innerHTML = modalHTML;
+
+  // Renderiza el modal
   modal.style.display = "flex";
 }
--- JS/RenderProjects.js
+++ JS/RenderProjects.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Renderizado");
   let projectsContainer = document.getElementById("projects__container");
 
   projectsContainer.innerHTML = "";
 
   let projectsHTML = projects
-    .map((project) => {
+    .map((project, index) => {
       return `
         <article class="projects__project">
             <img src="${project.imgURL}" alt="Imagen del proyecto" />
             <div class="projects__projectRight">
             <h3 class="projects__projectTitle">${project.title}</h3>
 
-            <button id="projectModal" class="project__infoButton projectModal">More info...</button>
+            <button onClick="openModal(${index})" class="project__infoButton projectModal ">More info...</button>
 
             <div class="projects__projectRedirections">
                 <a href="${project.webPage}" class="projectLink__left projects__projectLink">Web Page</a>
