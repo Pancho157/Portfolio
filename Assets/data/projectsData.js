7e7c1d6238041f906ca7758c51333122b5a5abad
Francisco Muñiz <hoxocraft65@gmail.com>
2022-09-19 14:02:18 -0300

    Agregada la renderización de proyectos en español

--- Assets/data/projectsData.js
+++ Assets/data/projectsData.js
@@ -35,3 +35,41 @@ const projects = [
     technologies: "React Native - Firebase",
   },
 ];
+
+const projectsSpanish = [
+  {
+    title: "Products API REST",
+    technologies: "Express - JavaScript - HTML - CSS",
+    description:
+      "It's a server based on node.js and express that offers a REST API of products <br> The API incorporates the next endpoints using express router: <br>- GET '/api/productos' -> Returns all products <br>- GET '/api/productos/:id' -> Returns a specific product <br>- POST '/api/productos' -> Receibes and add a product, then it return the product with its id <br>- PUT '/api/productos/:id' -> Receives and actualise a product <br>- DELETE '/api/productos/:id' -> Deletes an specific product",
+    imgURL: "./Assets/Images/API-REST.JPG",
+    webPage: "#",
+    github: "#",
+  },
+  {
+    title: "Live Chat using sockets",
+    description: "#",
+    imgURL: "#",
+    webPage: "#",
+    github: "#",
+    technologies: "#",
+  },
+  {
+    title: "Games App",
+    description:
+      "It's a web page that has the objective of demonstrating the knowledge acquired in the CoderHouse HTML and CSS course <br>It's an educational app (with no specific topic loaded) for small childs <br>It's objective is to to get to the final stage and to do so you must read something on the app, have a multiple choice quiz about it and win a Game (if you fail you only play the game again)",
+    imgURL: "./Assets/Images/App de juegos.png",
+    webPage: "#",
+    github: "#",
+    technologies: "HTML - CSS - JS",
+  },
+  {
+    title: "Ecommerce",
+    description:
+      "It's a generic E-commerce app made in the React for a of CoderHouse.    <br>The design was of my own and it's functionalities were made while the ongoing of the course   <br>It takes products from a custom made collection of Firebase, also takes a single product from Firebase and displays a detailed page of them.    <br>It has a cart (detailed page and miniature at top-right) with stock control and saves the 'Purchase orders' on Firebase",
+    imgURL: "#",
+    webPage: "#",
+    github: "#",
+    technologies: "React Native - Firebase",
+  },
+];
---
+++ JS/RenderProjectsSpanish.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let projectsContainer = document.getElementById("projects__container");
+
+  projectsContainer.innerHTML = "";
+
+  let projectsHTML = projectsSpanish
+    .map((project, index) => {
+      return `
+          <article class="projects__project">
+              <img src="${project.imgURL}" alt="Imagen del proyecto" />
+              <div class="projects__projectRight">
+              <h3 class="projects__projectTitle">${project.title}</h3>
+  
+              <button onClick="openModal(${index})" class="project__infoButton projectModal ">More info...</button>
+  
+              <div class="projects__projectRedirections">
+                  <a href="${project.webPage}" class="projectLink__left projects__projectLink">Web Page</a>
+  
+                  <a href="${project.github}" class="projectLink__right projects__projectLink">GitHub</a>
+              </div>
+              </div>
+          </article>
+          `;
+    })
+    .join(" ");
+
+  projectsContainer.innerHTML += projectsHTML;
+});
