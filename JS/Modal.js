8f39a7790d7ba5d2ae1f48041f72ea5bd39d9bef
Francisco Muñiz <hoxocraft65@gmail.com>
2022-09-19 08:39:28 -0300

    Renderizando los proyectos desde el archivo y reorganizado el proyecto - Reparar el modal y el Spanich.html

--- JS/Modal.js
+++ JS/Modal.js
@@ -1,60 +1,21 @@
-var firstModal = document.getElementById("firstModal");
-var secondModal = document.getElementById("secondModal");
-var thirdModal = document.getElementById("thirdModal");
-var fourthModal = document.getElementById("fourthModal");
+var modal = document.getElementById("modal");
+var modalOpenButton = document.getElementsByClassName("projectModal")[0];
+var closeModalButton = document.getElementById("modal__closeBtn");
 
-var firstModalOpenButton = document.getElementById("firstProjectModal");
-var secondModalOpenButton = document.getElementById("seconsProjectModal");
-var thirdModalOpenButton = document.getElementById("thirdProjectModal");
-var fourthModalOpenButton = document.getElementById("fourthProjectModal");
-
-var closeModalButton1 = document.getElementsByClassName("modal__closeBtn")[0];
-var closeModalButton2 = document.getElementsByClassName("modal__closeBtn")[1];
-var closeModalButton3 = document.getElementsByClassName("modal__closeBtn")[2];
-var closeModalButton4 = document.getElementsByClassName("modal__closeBtn")[3];
-
-firstModalOpenButton.addEventListener("click", openFirstModal);
-secondModalOpenButton.addEventListener("click", openSecondModal);
-thirdModalOpenButton.addEventListener("click", openThirdModal);
-fourthModalOpenButton.addEventListener("click", openFourthModal);
-
-closeModalButton1.addEventListener("click", closeModal);
-closeModalButton2.addEventListener("click", closeModal);
-closeModalButton3.addEventListener("click", closeModal);
-closeModalButton4.addEventListener("click", closeModal);
+modalOpenButton.addEventListener("click", openModal);
+closeModalButton.addEventListener("click", closeModal);
 window.addEventListener("click", outsideClick);
 
 function closeModal() {
-  firstModal.style.display = "none";
-  secondModal.style.display = "none";
-  thirdModal.style.display = "none";
-  fourthModal.style.display = "none";
+  modal.style.display = "none";
 }
 
 function outsideClick(e) {
-  if (e.target == firstModal) {
-    firstModal.style.display = "none";
-  } else if (e.target == secondModal) {
-    secondModal.style.display = "none";
-  } else if (e.target == thirdModal) {
-    thirdModal.style.display = "none";
-  } else if (e.target == fourthModal) {
-    fourthModal.style.display = "none";
+  if (e.target == modal) {
+    modal.style.display = "none";
   }
 }
 
-function openFirstModal() {
-  firstModal.style.display = "flex";
-}
-
-function openSecondModal() {
-  secondModal.style.display = "flex";
-}
-
-function openThirdModal() {
-  thirdModal.style.display = "flex";
-}
-
-function openFourthModal() {
-  fourthModal.style.display = "flex";
+function openModal() {
+  modal.style.display = "flex";
 }
---
+++ JS/RenderProjects.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Renderizado");
+  let projectsContainer = document.getElementById("projects__container");
+
+  projectsContainer.innerHTML = "";
+
+  let projectsHTML = projects
+    .map((project) => {
+      return `
+        <article class="projects__project">
+            <img src="${project.imgURL}" alt="Imagen del proyecto" />
+            <div class="projects__projectRight">
+            <h3 class="projects__projectTitle">${project.title}</h3>
+
+            <button id="projectModal" class="project__infoButton projectModal">More info...</button>
+
+            <div class="projects__projectRedirections">
+                <a href="${project.webPage}" class="projectLink__left projects__projectLink">Web Page</a>
+
+                <a href="${project.github}" class="projectLink__right projects__projectLink">GitHub</a>
+            </div>
+            </div>
+        </article>
+        `;
+    })
+    .join(" ");
+
+  projectsContainer.innerHTML += projectsHTML;
+});
