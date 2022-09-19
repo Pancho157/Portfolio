609f4cc7a1878249012d3912143bb5b5cf433d2e
Francisco Muñiz <hoxocraft65@gmail.com>
2022-09-19 13:19:29 -0300

    Reparados los popUp

--- JS/Modal.js
+++ JS/Modal.js
@@ -1,3 +1,6 @@
+var modal = document.getElementById("modal");
+window.addEventListener("click", outsideClick);
+
 function closeModal() {
   modal.style.display = "none";
 }
@@ -9,43 +12,38 @@ function outsideClick(e) {
 }
 
 function openModal(index) {
-  var modal = document.getElementById("modal");
-  modal.innerHTML = "";
-
-  const modalHTML = `
+  console.log(modal);
+  modal.innerHTML = `
     <div class="projects__modalContent">
-      <span class="modal__closeBtn" id="modal__closeBtn">&times;</span>
+      <span class="modal__closeBtn" onClick="closeModal()">&times;</span>
+    
+      <img src="${projects[index].imgURL}" alt="Imagen del proyecto" class="projects__modalImg"/>
+    
+      <div class="projects__modalRight">
+      <h3 class="projects__projectTitle">API REST</h3>
+    
+        <div class="projects__projectRedirections">
+          <a href="${projects[index].webPage}" class="projectLink__left projects__projectLink">Web Page</a>
 
-        <img src="${projects[index].imgURL}" alt="Imagen del proyecto" class="projects__modalImg"/>
-
-        <div class  ="projects__modalRight">
-
-          <h3 class="projects__projectTitle">API REST</h3>
-
-          <div class="projects__projectRedirections">
-            <a href="${projects[index].webPage}" class="projectLink__left projects__projectLink">Web Page</a>
-
-            <a href="${projects[index].github}" class="projectLink__right projects__projectLink">GitHub</a>
-          </div>
-
-          <hr />
-
-          <p class="projects__projectText projects__centerText">This project uses: </p>
-
-          <p class="projects__projectText projects__centerText">
-            ${projects[index].technologies}
-          </p>
-
-          <hr />
-
-          <p class="projects__projectText">
-            ${projects[index].description}
-          </p>
+          <a href="${projects[index].github}" class="projectLink__right projects__projectLink">GitHub</a>
         </div>
-      </div>
-  `;
 
-  modal.innerHTML = modalHTML;
+        <hr />
+            
+        <p class="projects__projectText projects__centerText">This project uses: </p>
+            
+        <p class="projects__projectText projects__centerText">
+          ${projects[index].technologies}
+        </p>
+            
+        <hr />
+            
+        <p class="projects__projectText">
+          ${projects[index].description}
+        </p>
+      </div>
+    </div>
+  `;
 
   // Renderiza el modal
   modal.style.display = "flex";
